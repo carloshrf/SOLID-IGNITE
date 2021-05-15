33621bc074363d796ba829f7cd8ba9f57d133748
carloshrf <carlos_6282010@hotmail.com>
2021-05-15 11:17:49 -0300

    swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerFile from './swagger.json';
+import swaggerUi from 'swagger-ui-express';
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -6,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/users", usersRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/users', usersRoutes);
 
 export { app };
